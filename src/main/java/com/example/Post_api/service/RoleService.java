3140600bcfe312b719bac07ac5e4143387ef3ac6
Federico <111809343+FedeTejada@users.noreply.github.com>
2025-09-14 19:56:19 -0300

    service user ready

--- src/main/java/com/example/Post_api/service/RoleService.java
+++ src/main/java/com/example/Post_api/service/RoleService.java
@@ -1,35 +1,65 @@
 package com.example.Post_api.service;
 
 import com.example.Post_api.entity.Role;
+import com.example.Post_api.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService implements IRoleService{
+    @Autowired
+    RoleRepository roleRepository;
+
     @Override
     public Role createRole(Role newRole) {
-
-        return null;
+        if (newRole.getPermissions() == null || newRole.getPermissions().isEmpty()) {
+            throw new IllegalArgumentException("Role must have at least one permission");
+        }
+        Role role = new Role();
+        role.setNameRole(newRole.getNameRole());
+        role.setPermissions(newRole.getPermissions());
+        roleRepository.save(role);
+        return role;
     }
 
     @Override
     public Role editRole(Long id, Role role) {
-        return null;
+        Optional<Role> opt =  roleRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Role with id: " + id + "not found");
+        }
+        Role role1 = opt.get();
+        role1.setNameRole(role.getNameRole());
+        role1.setPermissions(role.getPermissions());
+        roleRepository.save(role1);
+        return role1;
     }
 
     @Override
     public List<Role> getAllRole() {
-        return List.of();
+        return roleRepository.findAll();
     }
 
     @Override
     public Role getRole(Long id) {
-        return null;
+        Optional<Role> opt =  roleRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Role with id: " + id + "not found");
+        }
+        return opt.get();
     }
 
     @Override
     public Role deleteRole(Long id) {
-        return null;
+        Optional<Role> opt =  roleRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Role with id: " + id + "not found");
+        }
+        Role oldRole = opt.get();
+        roleRepository.deleteById(id);
+        return oldRole;
     }
 }
--- src/main/java/com/example/Post_api/service/UserService.java
+++ src/main/java/com/example/Post_api/service/UserService.java
@@ -1,34 +1,73 @@
 package com.example.Post_api.service;
 
 import com.example.Post_api.entity.User;
+import com.example.Post_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
+    @Autowired
+    UserRepository userRepository;
     @Override
     public void createUser(User newUser) {
-
+        if (newUser.getRoles().isEmpty() || newUser.getRoles() == null){
+            throw new IllegalArgumentException("User must have at least one Role");
+        }
+        User user = new User();
+        user.setUsername(newUser.getUsername());
+        user.setPassword(newUser.getPassword());
+        user.setEnable(newUser.isEnable());
+        user.setAccountNotExpired(newUser.isAccountNotExpired());
+        user.setAccountNotLocked(newUser.isAccountNotLocked());
+        user.setCredentialNotExpired(newUser.isCredentialNotExpired());
+        userRepository.save(user);
     }
 
     @Override
     public void editUser(Long id, User user) {
-
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Not found user by id:" + id);
+        }
+        if (user.getRoles().isEmpty() || user.getRoles() == null){
+            throw new IllegalArgumentException("User must have at least one Role");
+        }
+        User user1 = new User();
+        user1.setUsername(user.getUsername());
+        user1.setPassword(user.getPassword());
+        user1.setEnable(user.isEnable());
+        user1.setAccountNotExpired(user.isAccountNotExpired());
+        user1.setAccountNotLocked(user.isAccountNotLocked());
+        user1.setCredentialNotExpired(user.isCredentialNotExpired());
+        userRepository.save(user);
     }
 
     @Override
     public List<User> getAllUser() {
-        return List.of();
+        return userRepository.findAll();
     }
 
     @Override
     public User getUser(Long id) {
-        return null;
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Not found user by id:" + id);
+        }
+
+        return opt.get();
     }
 
     @Override
     public void deleteUser(Long id) {
-
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isEmpty()){
+            throw new IllegalArgumentException("Not found user by id:" + id);
+        }
+        userRepository.deleteById(id);
     }
 }
